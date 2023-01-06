bf3bc2e97e98d860204d943b67da8c711d509964
gigga <gigga@DESKTOP-D1LBEMK>
2023-01-07 00:38:53 +0100

    cartaGioco

---
+++ src/progcombattimenti/Carta.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package progcombattimenti;
+
+/**
+ *
+ * @author gigga
+ */
+public class Carta {
+    private String nome;
+    private int vita;
+    private String ambiente;
+}
---
+++ src/progcombattimenti/ProgCombattimenti.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package progcombattimenti;
+
+/**
+ *
+ * @author gigga
+ */
+public class ProgCombattimenti {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        GUICombattimenti gui = new GUICombattimenti();
+        gui.setVisible(true);
+    }
+    
+}
